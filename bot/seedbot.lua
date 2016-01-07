d83ca39db77be742093e17b2a0f40f9ac82a12e8
megatronzdspam <bot.zdspam@gmail.com>
2016-01-07 22:01:14 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,28 +220,15 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {37194141,104590943,0,tonumber(our_id)},--Sudo users
+    sudo_users = {76458212,95876809,179983320},--Sudo users
     disabled_channels = {},
-    realm = {94797781},--Realms Id
+    realm = {179983320},--Realms Id
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v1
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[Telemega v1
 
-https://github.com/SEEDTEAM/TeleSeed
 
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
 
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
 
-Our channels
-@teleseedch [English]
 ]],
     help_text = [[
 Commands list :
