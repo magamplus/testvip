831cfef3da650a14f640a5d01bc5a3cbd12c35e5
megatronzdspam <bot.zdspam@gmail.com>
2016-01-07 22:04:53 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return 'Telegram Bot '.. VERSION .. [[ 
+  amir/amir/abas
+end
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+end
